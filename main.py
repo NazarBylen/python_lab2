086d2347fb912f92ade259f9128c826c4b7d8164
NazarBylen <nazar.bylen.ir.2021@lpnu.ua>
2022-06-02 20:00:10 +0300

    naming repaired

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ complex3 = complex_num('a', 'bi')
 
 new_stack = stack_funcs()
 
-new_stack.add_elem(complex1)
-new_stack.add_elem(complex2)
+new_stack.add_element(complex1)
+new_stack.add_element(complex2)
 
 
 
 def main():
-    new_stack.add_elem(complex3)
+    new_stack.add_element(complex3)
     #new_stack.find_by_key(1)
-    #new_stack.print_elem_data(2)
+    #new_stack.print_element_data(2)
     #new_stack.delete_by_key(2)
     #new_stack.delete_stack()
-    #new_stack.sum_stack()
-    #new_stack.find_elem('a + bi')
+    #new_stack.sum_stack_elemets()
+    #new_stack.find_element('a + bi')
     #new_stack.find_stack_size()
     new_stack.print_all()
 
--- stack_funcs.py
+++ stack_funcs.py
@@ -5,49 +5,49 @@ class stack_funcs():
     global stack
     stack=[]
 
-    def add_elem(self, new_elem) -> stack:
+    def add_element(self, new_elem) -> stack:
         stack.append(new_elem)
         return stack
 
-    def find_by_key(self, elem_num: int) -> None:
-        for i in range(len(stack)):
-            if i == elem_num:
-                print(f'element under this number is : {stack[i]}')
+    def find_by_key(self, element_num: int) -> None:
+        for index in range(len(stack)):
+            if index == element_num:
+                print(f'element under this number is : {stack[index]}')
 
-    def print_elem_data(self, elem_num: int) -> None:
-        for i in range(len(stack)):
-            if i == elem_num:
-                print(stack[i].real)
-                print(stack[i].nreal)
+    def print_element_data(self, element_num: int) -> None:
+        for index in range(len(stack)):
+            if index == element_num:
+                print(stack[index].real)
+                print(stack[index].nreal)
 
-    def delete_by_key(self, elem_num: int) -> None:
-        for idx in range(len(stack)):
-            if idx == elem_num:
-                stack.pop(idx)
+    def delete_by_key(self, element_num: int) -> None:
+        for index in range(len(stack)):
+            if index == element_num:
+                stack.pop(index)
 
     def print_all(self) -> None:
-        for idx in range(len(stack)):
-            print(stack[idx])
+        for index in range(len(stack)):
+            print(stack[index])
 
     def delete_stack(self) -> None:
         return stack.clear()
 
-    def sum_stack(self) -> str:
+    def sum_stack_elemets(self) -> str:
         result_array = []
-        for i in range(len(stack)):
-            if i == len(stack) - 2:
-                result_array.append(stack[i].__str__())
-            if i == len(stack) - 1:
-                result_array.append(stack[i].__str__())
+        for index in range(len(stack)):
+            if index == len(stack) - 2:
+                result_array.append(stack[index].__str__())
+            if index == len(stack) - 1:
+                result_array.append(stack[index].__str__())
 
         result = result_array[0] + result_array[1]
         print(result_array)
         return result
 
-    def find_elem(self, elem: str) -> str:
-        for i in range(len(stack)):
-            if elem == stack[i].__str__():
-                print(f'elem found, its number is {i}')
+    def find_element(self, element: str) -> str:
+        for index in range(len(stack)):
+            if element == stack[index].__str__():
+                print(f'element found, its number is {index}')
 
     def find_stack_size(self) -> int:
         size = stack.__sizeof__()
